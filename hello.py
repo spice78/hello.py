5f887198343bab38b0f598fbfb3fe984701c8be3
spice78 <spicerama@gmail.com>
2021-04-06 11:55:11 +0300

    freedom
    Signed-off-by: spice78 <spicerama@gmail.com>

--- hello.py
+++ hello.py
@@ -1,9 +1,12 @@
 x = int(input ())
+spend = 13000
+age = 38
+month = 12
 
-if x<85:
-    print('cake')
-elif x>85:
-    print('uncake')
-else:
-    print ('Vasya vache')
-input()
+y = x/(spend*month)
+d = spend*month
+f = age+y
+
+print("in year spend %4d" % d)
+print("max age %4d" % f)
+print("year %4d" % y)
