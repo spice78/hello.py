24e54b99373cb98e9987af6a5ba8c21e5c2fd806
spice78 <spicerama@gmail.com>
2021-04-06 13:33:27 +0300

    ssss
    Signed-off-by: spice78 <spicerama@gmail.com>

--- hello.py
+++ hello.py
@@ -1,7 +1,15 @@
-# x = int(input ())exit
-x =85
-if x<=85:
-    print('cake')
-if x>=85:
-    print('uncake')
-input()
+print('Enter start: ')
+x = int(input ())
+print('Enter cycle: ')
+y = int(input ())
+z = 0
+
+x = x - 20000
+z = x*y
+#for i in range(y):
+#    z += x
+#    x += 1
+print (z)
+print((7000000 - (z + 1300000 + 3500000))/480000)
+
+#input()
