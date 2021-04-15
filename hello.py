53953f1dc5050fb6c78c1c55e698431658ed672f
spice78 <spicerama@gmail.com>
2021-04-15 20:15:20 +0300

    new needs
    Signed-off-by: spice78 <spicerama@gmail.com>

--- hello.py
+++ hello.py
@@ -1,17 +1,13 @@
-p40 = 25_000
-ears = 8_000
-watch = 8_000
-foto = 15_000
-mate = 30_000
+p40 = 25000
+foto = 15000
+mate = 30000
 
-sum = p40+ears+watch+foto+mate
+sum = p40+foto+mate
 quater = sum*0.75
 
 print('full price = %d' % sum)
 print('quater price = %d' % quater)
 
 print('p40 = %d' % (p40*0.75))
-print('ears = %d' % (ears*0.75))
-print('watch = %d' % (watch*0.75))
 print('foto = %d' % (foto*0.75))
 print('mate = %d' % (mate*0.75))
