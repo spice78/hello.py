55c3fc77e45f963f742d962aa6dd6f79c7c792f2
spice-home <spicerama@gmail.com>
2022-04-22 17:04:45 +0300

    add reverse method hz

--- hello.py
+++ hello.py
@@ -4,9 +4,14 @@ import calendar
 today_to = datetime.today().day
 today = calendar.day_name[datetime.today().weekday()]
 month = calendar.month_name[datetime.today().month]
+sasi = 'sasi'
 
 print(today, today_to, month, datetime.today().year)
 if today == 'Sunday':
     print('Fuck YEAH')
 else:
-    print('sasi')
+    print(sasi.capitalize())
+
+s = 'Koshka'[::-1].capitalize()
+
+print(''.join(reversed(s)).capitalize())
